ca9c51f6a5f11caf54acee32006ccd8a7e727dc9
Lucas Amancio <47485994+lulukc@users.noreply.github.com>
2021-05-18 23:04:35 -0300

    criando projeto spring

---
+++ src/main/java/br/com/fiap/antfy/antfy_backend/AntfyBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.antfy.antfy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntfyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntfyBackendApplication.class, args);
+	}
+
+}
